f425858d2da5ca6914bd48aee1c8c47ddd1e2e54
sborzic <sborzic@gmail.com>
2021-01-24 10:36:15 +0100

    finished cart

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
 const list = document.getElementById('list');
 const scrollUp = document.getElementById('scroll-up');
 const scrollDown = document.getElementById('scroll-down');
-
+const cartButton = document.querySelector('.cart-button');
 
 const scroll = (event)=>{
-  const direction = event.target.innerHTML;
-  if (direction === 'DOWN') {
-    list.scrollBy(0, 121);
+  const direction = event.target.id;
+  if (direction === 'scroll-up') {
+    list.scrollBy(0, -81);
   } else {
-    list.scrollBy(0, -121);
-  }
-  
+    list.scrollBy(0, 81);
+  }  
+}
+
+const toggleCart = ()=>{
+  const cart = document.querySelector('.list-wrapper');
+  cart.classList.toggle('show-cart');
 }
 
 scrollUp.addEventListener('click', scroll);
-scrollDown.addEventListener('click', scroll)
+scrollDown.addEventListener('click', scroll);
+cartButton.addEventListener('click', toggleCart)
 
